088e76ab2825ac7d2ab8666538b4fa2d04e7033d
zenrsr <ragasandeepreddy25@gmail.com>
2023-10-12 08:27:01 +0000

    first commit

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const AppointmentItem = props => {
+  const {starToggleLike, appointmentDetails} = props
+  const {id, title, date, isStarred} = appointmentDetails
+  const starImage = isStarred
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const toggleLike = () => {
+    starToggleLike(id)
+  }
+
+  return (
+    <li className="list-item">
+      <div className="appointment-info">
+        <p className="item-title">{title}</p>
+        <p className="item-date">Date: {date}</p>
+      </div>
+      <button type="button" className="star-button" onClick={toggleLike}>
+        <img src={starImage} alt="star" className="star-image" />
+      </button>
+    </li>
+  )
+}
+export default AppointmentItem
---
+++ src/components/Appointments/index.js
@@ -0,0 +1,128 @@
+import {Component} from 'react'
+import {format} from 'date-fns'
+import {v4} from 'uuid'
+import AppointmentItem from '../AppointmentItem'
+import './index.css'
+
+class Appointments extends Component {
+  state = {
+    nameInput: '',
+    dateInput: '',
+    appointmentList: [],
+    starButton: false,
+  }
+
+  addAppointment = event => {
+    event.preventDefault()
+    const {nameInput, dateInput, appointmentList} = this.state
+
+    const newApp = {
+      id: v4(),
+      title: nameInput,
+      date: format(new Date(dateInput), 'dd MMMM yyyy, EEEE'),
+      isStarred: false,
+    }
+    this.setState(prevState => ({
+      appointmentList: [...prevState.appointmentList, newApp],
+      nameInput: '',
+      dateInput: '',
+    }))
+  }
+
+  onChangeTitle = event => {
+    this.setState({nameInput: event.target.value})
+  }
+
+  onChangeDate = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  starToggleLike = id => {
+    // const {appointmentList} = this.state
+    this.setState(prevState => ({
+      appointmentList: prevState.appointmentList.map(eachApp => {
+        if (eachApp.id === id) {
+          return {...eachApp, isStarred: !eachApp.isStarred}
+        }
+        return eachApp
+      }),
+    }))
+  }
+
+  toggleStarred = () => {
+    // const {appointmentList} = this.state
+    this.setState(prevState => ({starButton: !prevState.starButton}))
+  }
+
+  renderAppointmentList = requiredList =>
+    requiredList.map(eachAppointment => (
+      <AppointmentItem
+        appointmentDetails={eachAppointment}
+        key={eachAppointment.id}
+        starToggleLike={this.starToggleLike}
+      />
+    ))
+
+  render() {
+    const {nameInput, dateInput, appointmentList, starButton} = this.state
+    const filteredList = {
+      appointmentList: appointmentList.filter(each => each.isStarred === true),
+    }
+    const activeStarredButton = starButton ? 'active' : ''
+
+    return (
+      <div className="background-container">
+        <div className="container">
+          <div className="sub-container">
+            <div className="appointment-container">
+              <form className="form" onSubmit={this.addAppointment}>
+                <h1 className="heading"> Add Appointment</h1>
+                <p className="title">TITLE</p>
+                <input
+                  type="text"
+                  className="title-input"
+                  value={nameInput}
+                  placeholder="Title"
+                  onChange={this.onChangeTitle}
+                />
+                <p className="date">DATE</p>
+                <input
+                  type="date"
+                  className="date-input"
+                  value={dateInput}
+                  placeholder="dd/mm/yy"
+                  onChange={this.onChangeDate}
+                />
+                <button type="submit" className="add-button">
+                  Add
+                </button>
+              </form>
+            </div>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+              alt="appointments"
+              className="appointment-image"
+            />
+          </div>
+          <hr className="line" />
+          <div className="appointment-list-heading">
+            <h1 className="heading">Appointments</h1>
+            <button
+              type="button"
+              className={` starred-button ${activeStarredButton} `}
+              onClick={this.toggleStarred}
+            >
+              starred
+            </button>
+          </div>
+          <ul className="list-container" type="none">
+            {starButton
+              ? this.renderAppointmentList(filteredList)
+              : this.renderAppointmentList(appointmentList)}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default Appointments
